02694fd1abf6c47b15d86ffc6a4aafa4c6daed69
MariaBuslaeva <maria.singlefox@gmail.com>
2019-08-20 20:54:38 +0300

    lesson18

---
+++ app/Http/Controllers/AuthorsController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Author;
+use App\Http\Requests\StoreAuthorRequest;
+
+class AuthorsController extends Controller
+{
+    
+    public function store(StoreAuthorRequest $request)
+    {
+    	$data = $request->only(['name']);
+
+    	$model = new Author();
+    	$model->fill($data);
+    	$model->save();
+
+    	return response()->json([
+    		'success' => true
+    	]);
+    }
+
+    public function update(StoreAuthorRequest $request, Author $author)
+    {
+    	$data = $request->only(['name']);
+
+    	$author->fill($data);
+    	$author->save();
+
+    	return response()->json([
+    		'success' => true
+    	]);
+    }
+
+    public function delete(Author $author)
+    {
+    	$author->delete();
+
+    	return response()->json([
+    		'success' => true
+    	]);
+    }
+
+    public function index()
+    {
+    	$authors = Author::all();
+
+    	return $authors;
+    }
+
+    
+}
